50f614ce6a093b9767a28bc993252ee26865ff68
varun1729 <varun.narayanan1729@gmail.com>
2017-11-15 16:37:50 -0800

    improved it

--- 2.cpp
+++ 2.cpp
@@ -1,40 +1,24 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-
-long Fibo(long num, long array [40])
-{
-	if (num == 0 || num==1)
-		return 1;
-	else if (array[num]==0)
-		array[num] = Fibo(num-1, array) + Fibo(num-2, array);
-
-		return array[num];
-}
-
-
-long FiboEvenSum (long num)
-{
-	long array [40] = {0};
-	long sum = 0;
-	long i = 0;
-	while (Fibo(i,array)<num)
-	{
-		long a = Fibo(i,array);
-		if (a%2==0)
-		{
-			//cout<<a<<" ";
-			sum+=a;
-		}
-		i++;
-	}
-	return sum;
-}
 
 int main ()
 {
-	cout<<FiboEvenSum(40000000);
+	int val1 = 1;
+	int val2 = 2;
+	int sum = 2;
+	int temp;
+	while (val2 < 4000000)
+	{
+		temp = val2;
+		val2 += val1;
+		val1 = temp;
+		if (val2%2 == 0)
+		{
+			sum += val2;
+		}
+
+	}
+	cout << sum;
 	return 0;
 	
 }
