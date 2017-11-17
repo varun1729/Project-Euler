4c72f75272bb7173994226221e897929ed2854c6
varun1729 <varun.narayanan1729@gmail.com>
2017-11-16 16:47:10 -0800

    added 206

--- 206.py
+++ 206.py
@@ -1,4 +1,21 @@
-lb = 3162277660
-print (len("1_2_3_4_5_6_7_8_9_0"))
-print (len(str(10**18)))
+import math
+import sys
+number = "1_2_3_4_5_6_7_8_9_0"
 
+lb = 1000000000
+ub = 1414213562
+
+def isConcealed (x):
+	square = str(int(x**2))
+	i = 2
+	for y in range (2,17,2):
+		if (square[y] == str(i)):
+			i += 1
+		else:
+			return False
+	return True
+
+for x in range (1000000000,1414213562,10):
+	if (isConcealed(x)):
+		print (x)
+		sys.exit()
